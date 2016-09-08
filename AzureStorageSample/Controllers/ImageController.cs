2b9d3afa56490a3977d8f0b6c0dd1f65aa7db688
Ersin <Ersin@DESKTOP-JBE92FO>
2016-09-08 23:46:44 +0300

    Add project files.

---
+++ AzureStorageSample/Controllers/ImageController.cs
@@ -0,0 +1,34 @@
+﻿using AzureStorageSample.Storage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AzureStorageSample.Controllers
+{
+    public class ImageController : Controller
+    {
+        ImageService imageService = new ImageService();
+
+        // GET: Image
+        public ActionResult Index()
+        {
+            var LastestImage = string.Empty;
+            if(TempData["LastestImage"] != null)
+            {
+                ViewBag.LatestImage = Convert.ToString(TempData["LastestImage"]);
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Index(HttpPostedFileBase photo) 
+        {
+            var imageUrl = await imageService.UploadImageAsync(photo);
+            TempData["LastestImage"] = imageUrl.ToString();
+            return RedirectToAction("Index", "Image");
+        }
+    }
+}
---
+++ AzureStorageSample/Storage/ConnectionString.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Azure;
+using Microsoft.WindowsAzure.Storage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AzureStorageSample.Storage
+{
+    //  I will access the storage account name and storage access key and create the connection string for blob storage. 
+    public static class ConnectionString
+    {
+        static string account = CloudConfigurationManager.GetSetting("StorageAccountName");
+        static string key = CloudConfigurationManager.GetSetting("StorageAccountKey");
+        public static CloudStorageAccount GetConnectionString()
+        {
+            string connectionString = string.Format("DefaultEndpointsProtocol=https;AccountName={0};AccountKey={1}", account, key);
+            return CloudStorageAccount.Parse(connectionString);
+        }
+    }
+}
